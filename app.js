ef756fd737c757db18ba1bc899836d1fc68990ad
DiogoAlip <diogoalipazaga@gmail.com>
2024-05-16 12:01:32 -0500

    feat:icon, css added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const path = require("path");
+
 io.on("connection", (socket) => {
   console.log("User is Conected");
 
@@ -16,14 +18,12 @@ io.on("connection", (socket) => {
   socket.emit("initialData", {});
 });
 
+app.use(express.static(path.join(__dirname, "home")));
+app.use(express.static(path.join(__dirname, "src")));
+
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/home/index.html`);
 }); //conexion para profesor
-
-/*app.get("/alumno", (req, res) => {
-  res.sendFile(`${__dirname}/alumno/index.html`);
-  console.log(`${__dirname}`);
-}); //conexion para alumno */
 
 server.listen(3010, () => {
   console.log("servidor corriendo en el puerto http://localhost:3010");
